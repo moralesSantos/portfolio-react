e26c45d37a3b51ac7040a22e0ad1a302d035b494
luis <jmsantos879@gmail.com>
2025-02-21 16:33:48 -0500

    updated projects page

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -2,7 +2,7 @@ import coming_soon_img from '../assets/coming_soon.png'
 import amigoscode_img from '../assets/amigoscode.png'
 import dicegame_img from '../assets/dicegame.png'
 import simongame_img from '../assets/simon-game.png'
-import invenotry_img from '../assets/inventory_app.png'
+import inventory_img from '../assets/inventory_app.png'
 
 const mywork_data = [
     {
@@ -10,27 +10,26 @@ const mywork_data = [
         w_name:"Web design",
         w_img:amigoscode_img,
         w_link:"https://www.full-stack-react.luismwebsite.com/",
-    }, 
+    },
     {
         w_no:2,
         w_name:"Web design",
-        w_img:"https://inventory-frontend-flame-six.vercel.app/",
-        w_link:""
-    },
+        w_img:inventory_img,
+        w_link:"https://inventory-frontend-flame-six.vercel.app/"
+    }, 
     {
         w_no:3,
+        w_name:"Web design",
+        w_img:simongame_img,
+        w_link:"https://moralessantos.github.io/SimonGame/"
+    },
+    {
+        w_no:4,
         w_name:"Web design",
         w_img:dicegame_img,
         w_link:"https://moralessantos.github.io/DiceGame/"
         
     },
-    {
-        w_no:4,
-        w_name:"Web design",
-        w_img:simongame_img,
-        w_link:"https://moralessantos.github.io/SimonGame/"
-    },
-   
     {
         w_no:5,
         w_name:"Web design",
